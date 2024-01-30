1ae1caafd8f31ec051c2f7d261252a07bd155f9d
xfxpositions <xfxpositions@gmail.com>
2024-01-30 18:19:39 +0300

    new file opeartions added

--- src/utils.zig
+++ src/utils.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 
+/// Read all the file contents to buffer. Returns FileTooBig error if file size is bigger than buffer size.
 pub fn read_file_to_buffer(buffer: *[]u8, file_path: []const u8) !usize {
     var file = try std.fs.cwd().openFile(file_path, .{});
     defer file.close();
@@ -17,22 +18,33 @@ pub fn read_file_to_buffer(buffer: *[]u8, file_path: []const u8) !usize {
 
     return bytes_read;
 }
+
+/// Check if file exists, returns true if file exist.
+pub fn is_file_exist(file_path: []const u8) !bool {
+    _ = std.fs.cwd().statFile(file_path) catch |err| {
+        switch (err) {
+            error.FileNotFound => return false,
+            else => return err,
+        }
+    };
+    return true;
+}
+
+/// Writes whole buffer to file, creates file if it not exists
 pub fn write_buffer_to_file(
     file_path: []const u8,
     buffer: []const u8,
 ) !void {
-    _ = buffer; // autofix
+    const file_exists = try is_file_exist(file_path);
+
+    // Create file if it's not exists
+    if (!file_exists) {
+        _ = try std.fs.cwd().createFile(file_path, .{});
+    }
     var file = try std.fs.cwd().openFile(file_path, .{ .mode = std.fs.File.OpenMode.write_only });
-    const metadata = try file.metadata();
-    const kind = metadata.kind();
-    const stats = file.stat();
-    std.debug.print("stats: {any}", .{stats});
-
-    std.debug.print("kind of file is: {}\n", .{kind});
-
     defer file.close();
 
-    // _ = try file.writeAll(buffer);
+    _ = try file.writeAll(buffer);
 
     std.debug.print("Contents written to file {s}\n", .{file_path});
 }
