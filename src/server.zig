34f0e5fb2fc787dd7e4d47c72441c96d8378803d
xfxpositions <xfxpositions@gmail.com>
2024-01-30 18:19:57 +0300

    servr updated

--- src/server.zig
+++ src/server.zig
@@ -6,22 +6,49 @@ const util = @import("utils.zig");
 const port: u16 = 4000;
 const address = std.net.Address.initIp4(.{ 127, 0, 0, 1 }, port);
 
-fn handle_connection(connection: std.net.StreamServer.Connection) void {
+fn handle_connection_wrapper(connection: std.net.StreamServer.Connection, allocator: std.mem.Allocator) void {
+    handle_connection(connection, allocator) catch |err| {
+        std.debug.print("some error happened while handling connection: {any}\n", .{err});
+    };
+}
+
+fn handle_connection(connection: std.net.StreamServer.Connection, allocator: std.mem.Allocator) !void {
     std.debug.print("{any}\n", .{connection.address});
 
-    //Read with chunks
-    const buffer = try std.heap.page_allocator.alloc(u8, 1024);
+    const chunk_size: usize = 1024;
+    var chunks_n: u16 = 1; // How many chunks are allocated
 
-    const bytes_read = try connection.stream.readAll(buffer);
-    _ = bytes_read; // autofix
+    // Declare buffer for read
+    const buffer = try allocator.alloc(u8, chunk_size);
+    defer allocator.free(buffer);
+
+    //Read with chunks
+    while (true) {
+        const bytes_read = try connection.stream.read(buffer);
+        if (bytes_read == 0) {
+            break; // End of the stream
+        }
+
+        // Extend buffer size
+        chunks_n += 1;
+        _ = try allocator.realloc(buffer, chunk_size * chunks_n);
+
+        if (bytes_read < buffer.len) {
+            break; // All bytes read
+        }
+    }
+    std.debug.print("got out\n", .{});
 }
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
 
     // initialize the threadpool
     var pool: std.Thread.Pool = undefined;
 
-    _ = try pool.init(.{ .allocator = std.heap.page_allocator }); // Init pool with page_allocator.
+    _ = try pool.init(.{ .allocator = allocator }); // Init pool with page_allocator.
     defer pool.deinit();
 
     // Accept 16 User requests at the same time.
@@ -42,6 +69,6 @@ pub fn main() !void {
             std.debug.print("Some error happened while accepting the connection {any}\n", .{err});
             return;
         };
-        _ = try pool.spawn(handle_connection, .{connection});
+        _ = try pool.spawn(handle_connection_wrapper, .{ connection, allocator });
     }
 }
--- src/tesst.zig
+++
@@ -1,6 +0,0 @@
-const std = @import("std");
-const util = @import("utils.zig");
-
-pub fn main() !void {
-    _ = try util.write_buffer_to_file("./test2.txt", "kirwe kirwe kirwe\n");
-}
